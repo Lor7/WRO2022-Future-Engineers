a89b129bec3300a01f04995a115c57e6688a435c
Lor7 <104694969+Lor7@users.noreply.github.com>
2022-05-02 01:19:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from pybricks.tools import wait
 #from pixycamev3.pixy2 import Pixy2, MainFeatures
 
 ev3 = EV3Brick()
-#pixy2 = Pixy2(port = 1, i2c_address = 0x54)
+#pixy2 = Pixy2(port = 3, i2c_address = 0x54)
 motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
-steering = Motor(Port.A)
-#ir = InfraredSensor(Port.S1)
+steering = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+#ir = InfraredSensor(Port.S4)
 cs = ColorSensor(Port.S2)
 gs = GyroSensor(Port.S1)
 watch = StopWatch()
@@ -22,6 +22,7 @@ watch = StopWatch()
 """ UTILS """
 
 def getDistance():
+    print("Distance:", ir.distance())
     return ir.distace()
 
 def getColor():
@@ -29,42 +30,40 @@ def getColor():
     return cs.color()
 
 def getAngle():
-    global quarterLap
-    print("Angle:", gs.angle()/quarterLap)
-    return gs.angle() / quarterLap            #Switch Return(s)
-    #return 90  / quarterLap
+    global quarterLap, k
+    print("Angle:", gs.angle()-(quarterLap * 90 * k))
+    return gs.angle() - (quarterLap * 90 * k)            #
+    #return 90  -  (quarterLap * 90)
 
 def goStraightOn():
-    global steeringSpeed, lastCorrection
-    steering.run_angle(steeringSpeed, -lastCorrection, wait = True)
-    correction = -getAngle()
+    global steeringSpeed, lastCorrection, quarterLap
+    if ( quarterLap == 0 or quarterLap == 2 or quarterLap == 3 or quarterLap == 1): ###
+        correction = - lastCorrection - getAngle()
+    elif ( quarterLap ):
+        correction = - getAngle()
+
     lastCorrection = correction
     print("Correction:", correction)
-    steering.run_angle(steeringSpeed, correction, wait = False)
+    steering.run_angle(steeringSpeed, correction, wait = True)
 
 def turnBlueLine():
     global steeringSpeed, flag_line, quarterLap
-    currentSteeringAngle = steering.angle()
-    steering.run_angle( steeringSpeed, -currentSteeringAngle, wait = True) # Closing (If there's something opened or a value to correct)
     flag_line = True
-    steeringAngle = 30
+    steeringAngle = 40
     steering.run_angle(steeringSpeed, steeringAngle, wait = False) # Opening
     while (getAngle() < 88):
-        wait(100)
-    steering.run_angle(steeringSpeed, -steeringAngle, wait = False) # Closing
+        wait(80)
+    steering.run_angle(steeringSpeed, -steeringAngle, wait = True) # Closing
     quarterLap = quarterLap + 1
 
 def turnOrangeLine():
     global steeringSpeed, flag_line, quarterLap
-    currentSteeringAngle = steering.angle()
-    print("Current steering angle:", currentSteeringAngle)
-    steering.run_angle( steeringSpeed, -currentSteeringAngle, wait = True) # Closing (If there's something opened or a value to correct)
     flag_line = True
-    steeringAngle = -10
+    steeringAngle = -40
     steering.run_angle(steeringSpeed, steeringAngle, wait = False) # Opening
     while (getAngle() > -88):
-        wait(100)
-    steering.run_angle(steeringSpeed, -steeringAngle, wait = False) # Closing
+        wait(80)
+    steering.run_angle(steeringSpeed, -steeringAngle, wait = True) # Closing
     quarterLap = quarterLap + 1
 
 def takeAnAction():
@@ -83,19 +82,20 @@ def takeAnAction():
 """ ALGORITHM """
 
 # Avvio la propulsione
-motor.run(1000)
+#motor.run(1000)
 
 # Initializations
-steeringSpeed = 25
+steeringSpeed = 150
 motorSpeed = 1000
 watch.reset()
 gs.reset_angle(0)
 flag_line = False
 blocks = False
 lastCorrection = 0
-quarterLap = 1
+quarterLap = 0
 lapCompleted = 0
 action = ""
+k = 1
 # Ciclo equivalente al loop() di uno sketch Arduino
 while True:
 
@@ -105,20 +105,22 @@ while True:
     # Color Lines
     if (color == Color.RED):
         action = "onOrangeLine"
+        k = -1
     elif (color == Color.BLUE):
         action = "onBlueLine"
+        k = 1
     # Pixy
     elif (blocks):
         pass
     # Just go straight
-    if (True):
+    elif (True):
         action = "moveStraight"
 
 
     print(action)
     takeAnAction()
-
-    if (quarterLap == 4):
+    print("Quarter Lap:", quarterLap)
+    if (quarterLap == 3):
         quarterLap = 1
         lapCompleted = lapCompleted + 1
 
